caa0f63483a5e2183ebfe7d7fa588f5d66b0a98b
LochanRn <rajanaga_ra@srmuniv.edu.in>
2019-04-05 22:50:53 +0530

    removed templates from public

---
+++ jsonData/feedBack.js
@@ -0,0 +1,4 @@
+var dataBas = {
+    "people":{ "OracleId":"","FirstName":"John","LastName":"Doe","EmailId":"john@gmail.com",
+     "gender":"male", "password":"lochan123","confirmPassword":"lochan123"}
+    };
