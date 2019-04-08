bc17218c7f8fb8ecdd7d67610acd0d01dd83cf05
LochanRn <rajanaga_ra@srmuniv.edu.in>
2019-04-08 16:37:02 +0530

    fixing my issues

--- public/jsonData/feedBack.js
+++ public/jsonData/feedBack.js
@@ -1,3 +1,21 @@
 var feedBack = [
+    {
+        "OracleId":1238,
+        "GrowthAreas":"something",
+        "Suggestions":"nothing",
+        "Ratings":5
+    },
+    {
+        "OracleId":1238,
+        "GrowthAreas":"something",
+        "Suggestions":"nothing",
+        "Ratings":5
+    },
+    {
+        "OracleId":1239,
+        "GrowthAreas":"something",
+        "Suggestions":"nothing",
+        "Ratings":5
+    },
      
 ];
