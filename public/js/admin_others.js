870fe8d19dedeb8e130358ae2147d35a25a3a97b
Abhishek Jha <abhjha3@publicisgroupe.net>
2019-04-08 16:47:16 +0530

    ad

--- public/js/admin_others.js
+++ public/js/admin_others.js
@@ -1,3 +1,44 @@
-function resetFunction() {
-    document.getElementById("registerForm").reset();
+
+
+    //let dataBase = window.dataBase;
+    //let requests = window.requests;
+
+
+function Validate() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    console.log(password, confirmPassword);
+    if (password != confirmPassword) {
+        alert("Passwords do not match.");
+        return false;
+    }
+
+    window.dataBase.push({
+        "OracleId": "0000",
+        "FirstName": document.getElementById("firstName").value,
+        "LastName": document.getElementById("lastName").value,
+        "EmailId": document.getElementById("email").value,
+        "gender": document.getElementsByName("gender").value,
+        "password": document.getElementById("password").value,
+        "role": document.getElementById("role").value;
+    });
+
+
+    console.log(dataBase)
+
+    return true;
 }
+
+
+    //admin_pm k andar form waala
+    function resetFunction() {
+        document.getElementById("registerForm").reset();
+    }
+
+    console.log(dataBase);
+
+
+
+
+
+
